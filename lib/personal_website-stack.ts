f6c4465d3495de79117d27cee3925947586c8bac
Chris Lucas <chris@chriswlucas.com>
2022-04-17 02:20:25 -0700

    migrate from cdk cloudfront web distribution to standard distribution

--- lib/personal_website-stack.ts
+++ lib/personal_website-stack.ts
@@ -1,12 +1,16 @@
 import { Construct } from 'constructs';
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
+
 import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { aws_route53 as route53 } from 'aws-cdk-lib';
 import { aws_route53_targets as route53_targets } from 'aws-cdk-lib';
 import { aws_cloudfront as cloudfront } from 'aws-cdk-lib';
+import { aws_cloudfront_origins as origins } from 'aws-cdk-lib';
 import { aws_certificatemanager as acm } from 'aws-cdk-lib';
 
-interface ZoneMap { [index: string]: route53.PublicHostedZone }
+import { AllowedMethods, CachedMethods, SecurityPolicyProtocol, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+
+interface DomainZoneMap { [index: string]: route53.PublicHostedZone }
 
 export class PersonalWebsiteStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -18,17 +22,17 @@ export class PersonalWebsiteStack extends Stack {
     const apexDomains = [primaryDomain, ...secondaryDomains]
 
     // Hosted Zones
-    const zoneMap = apexDomains
+    const domainZoneMap = apexDomains
       .reduce(
-        (object, domain) => {
+        (map, domain) => {
 
-          object[domain] = new route53.PublicHostedZone(this, domain, {
+          map[domain] = new route53.PublicHostedZone(this, domain, {
             zoneName: domain
           });
 
-          return object
+          return map
         },
-        {} as ZoneMap,
+        {} as DomainZoneMap,
       )
 
     // Website
@@ -42,36 +46,41 @@ export class PersonalWebsiteStack extends Stack {
       objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED
     })
 
-    const oai = new cloudfront.OriginAccessIdentity(this, 'OAI');
+    // Access logs
+    const accessLogBucket = new s3.Bucket(this, `${primaryDomain}-access-logs-bucket`, {
+      bucketName: `${primaryDomain}-access-logs`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      lifecycleRules: [ { expiration: Duration.days(90) } ],
+    })
 
+    // Certificate
     const certificate = new acm.Certificate(this, `${primaryDomain}-certificate`, {
       domainName: primaryDomain,
       subjectAlternativeNames: websiteDomains.filter(domain => domain !== primaryDomain),
     })
 
-    const viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
+    // Distribution
+    const distribution = new cloudfront.Distribution(this, `${primaryDomain}-distribution`, {
+      enabled: true,
       certificate,
-      {
-        aliases: websiteDomains,
-        sslMethod: cloudfront.SSLMethod.SNI,
+      minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+      domainNames: websiteDomains,
+      defaultBehavior: {
+        origin: new origins.S3Origin(siteBucket),
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
+        cachedMethods: CachedMethods.CACHE_GET_HEAD,
+        compress: true,
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
-    )
 
-    const distribution = new cloudfront.CloudFrontWebDistribution(this, `${primaryDomain}-distribution`, {
-      viewerCertificate,
-      originConfigs: [
-        {
-          s3OriginSource: { 
-            s3BucketSource: siteBucket,
-            originAccessIdentity: oai,
-          },
-          behaviors: [{
-              isDefaultBehavior: true,
-              compress: true,
-          }]
-        }
-      ],
-      priceClass: cloudfront.PriceClass.PRICE_CLASS_100
+      defaultRootObject: "index.html",
+      enableIpv6: true,
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+
+      // logging
+      logBucket: accessLogBucket,
+      logIncludesCookies: false,
     })
 
     // www subdomain redirection
@@ -79,7 +88,7 @@ export class PersonalWebsiteStack extends Stack {
       const apexDomain = apexDomains.filter(apexDomain => domain.includes(apexDomain))[0]
 
       new route53.ARecord(this, `${domain}-cf-aliases`, {
-        zone: zoneMap[apexDomain],
+        zone: domainZoneMap[apexDomain],
         recordName: domain,
         target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(distribution)),
       })
@@ -88,7 +97,7 @@ export class PersonalWebsiteStack extends Stack {
     // Email
     apexDomains.forEach(domain => {
       new route53.MxRecord(this, `${domain}-mx-gmail`, {
-        zone: zoneMap[domain],
+        zone: domainZoneMap[domain],
         values: [
           { hostName: 'ASPMX.L.GOOGLE.COM.', priority: 1 },
           { hostName: 'ALT1.ASPMX.L.GOOGLE.COM.', priority: 5 },
@@ -99,7 +108,7 @@ export class PersonalWebsiteStack extends Stack {
       })
 
       new route53.TxtRecord(this, `${domain}-txt-spf`, {
-        zone: zoneMap[domain],
+        zone: domainZoneMap[domain],
         values: ['v=spf1 include:_spf.google.com ~all']
       })
     })
