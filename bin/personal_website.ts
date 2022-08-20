d2b70208ac4eb7f53f134d7e49d62b88e8ba1d09
Chris <chris@chriswlucas.com>
2022-08-20 16:37:31 -0700

    updates record TTLs

--- bin/personal_website.ts
+++ bin/personal_website.ts
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
-import { App } from 'aws-cdk-lib';
+import { App, Duration } from 'aws-cdk-lib';
 import { PersonalWebsiteStack } from '../lib/personal_website-stack';
 
 const app = new App();
 
+const iCloudSpfValue = 'v=spf1 include:icloud.com ~all'
 const iCloudMxValues = [
   { hostName: 'mx01.mail.icloud.com.', priority: 10 },
   { hostName: 'mx02.mail.icloud.com.', priority: 10 },
 ]
-
-const iCloudSpfValue = 'v=spf1 include:icloud.com ~all'
 
 new PersonalWebsiteStack(app, 'PersonalWebsiteStack', {
   env: {
@@ -19,10 +18,16 @@ new PersonalWebsiteStack(app, 'PersonalWebsiteStack', {
   primaryDomainConfig: {
     domain: 'chriswlucas.com',
     subdomainMxRecords: {
-      '': { values: iCloudMxValues },
+      '': {
+        values: iCloudMxValues,
+        ttl: Duration.days(1),
+      },
     },
     subdomainCnameRecords: {
-      'sig1._domainkey': { domainName: 'sig1.dkim.chriswlucas.com.at.icloudmailadmin.com.' },
+      'sig1._domainkey': {
+        domainName: 'sig1.dkim.chriswlucas.com.at.icloudmailadmin.com.',
+        ttl: Duration.days(1),
+      },
     },
     subdomainTxtRecords: {
       '': {
@@ -30,7 +35,8 @@ new PersonalWebsiteStack(app, 'PersonalWebsiteStack', {
           iCloudSpfValue,
           'apple-domain=DdleKqlDev7mc5xo',
           'keybase-site-verification=74xSzNnFzF37JGsYtlTgQ5ip70dKbUvAQLpHnaxiEp4',
-        ]
+        ],
+        ttl: Duration.hours(1),
       },
     },
   },
